bd5e39817fc1bb0d86ae21ba281a638ca8a5d067
LastOfUs13 <122573180+LastOfUs13@users.noreply.github.com>
2023-01-18 16:23:15 +0400

    Update task4.py

--- task4.py
+++ task4.py
@@ -6,3 +6,4 @@ omnivores_and_vegetables = {**omnivores, **vegetarians}
 for key, values in omnivores_and_vegetables.items():
     if values == 'vegetarian':
         print(key + ' will add to list of guests ')
+        
