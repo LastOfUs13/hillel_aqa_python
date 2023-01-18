dfb4ec159b740b7fdd35c5b26465523a2cb69b6e
misha_fox <usmishafox13@gmail.com>
2023-01-18 15:53:23 +0400

    task 3 commit

---
+++ task3.py
@@ -0,0 +1,6 @@
+casino_blacklist = {'Ben Affleck', 'Peter Parker', 'Selina Kyle'}
+poker_blacklist = {'Barry Allen', 'Ben Affleck', 'Selina Kyle'}
+alcohol_blacklist = {'Oliver Queen', 'Ben Affleck', 'Selina Kyle'}
+
+print(casino_blacklist.intersection(poker_blacklist, alcohol_blacklist))
+
