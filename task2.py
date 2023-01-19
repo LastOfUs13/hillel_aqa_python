bb6f7881e2ec7eeae2b198e2048643181c3396cb
misha_fox <usmishafox13@gmail.com>
2023-01-19 11:29:54 +0400

    fixes

--- task2.py
+++ task2.py
@@ -1,5 +1,11 @@
 Eleks = ['John', 'Michael', 'Peter', 'Oliver', 'Nina']
 Toshiba = ['John', 'Nina']
 Toshiba.extend(Eleks)
+<<<<<<< HEAD
 print(Toshiba)
 
+=======
+Eleks.clear()
+print(Toshiba)
+print(Eleks)
+>>>>>>> c1f9d0d (fixes)
