79076646109448b2fdbc0135af496ed2372319d4
misha_fox <usmishafox13@gmail.com>
2023-01-19 11:32:08 +0400

    last fixes

--- task2.py
+++ task2.py
@@ -1,11 +1,8 @@
 Eleks = ['John', 'Michael', 'Peter', 'Oliver', 'Nina']
 Toshiba = ['John', 'Nina']
 Toshiba.extend(Eleks)
-<<<<<<< HEAD
-print(Toshiba)
-
-=======
 Eleks.clear()
 print(Toshiba)
 print(Eleks)
->>>>>>> c1f9d0d (fixes)
+
+
--- task4.py
+++ task4.py
@@ -4,10 +4,5 @@ vegetarians = {'Sam': 'vegetarian', 'Kat': 'vegetarian'}
 omnivores_and_vegetables = {**omnivores, **vegetarians}
 
 for key, values in omnivores_and_vegetables.items():
-<<<<<<< HEAD
-    if values == 'vegetarian':
-        print(key + ' will add to list of guests ')
-        
-=======
     print(key + ' will add to list of guests ')
->>>>>>> c1f9d0d (fixes)
+
