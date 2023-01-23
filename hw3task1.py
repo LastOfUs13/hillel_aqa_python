6bc1fc37a1a248e8eca363803c393fc1edc91b97
misha_fox <usmishafox13@gmail.com>
2023-01-23 19:06:32 +0400

    with enumerate

--- hw3task1.py
+++ hw3task1.py
@@ -1,14 +1,15 @@
-mlist = [1, 2, 3, 4, 5, 6, 7, 8]
-even = []
-odd = []
-for index in range(len(mlist)):
+even_list = []
+odd_list = []
+
+some = [1, 2, 3, 4, 5, 6, 7, 8]
+for index, numbers in enumerate(some):
     if index % 2 == 0:
-        even_tuple = ("index - " + str(index) + ", key- " + str(mlist[index]))
-        even.append(even_tuple)
+        even_indexes = (index, numbers)
+        even_list.append(even_indexes)
 
     else:
-        odd_tuple = ("index - " + str(index) + ", key-" + str(mlist[index]))
-        odd.append(odd_tuple)
+        odd_indexes = (index, numbers)
+        odd_list.append(odd_indexes)
 
-print('list of even indexes - ' + str(even))
-print('list of odd indexes - ' + str(odd))
+print('Here is even indexes - ' + str(even_list))
+print('Here is odd indexes- ' + str(odd_list))
