6cf45d71d28541753eadfb56259d66ab416eca45
misha_fox <usmishafox13@gmail.com>
2023-01-26 20:34:06 +0400

    small fix

--- hw4task3.py
+++ hw4task3.py
@@ -1,13 +1,5 @@
 long_string = "name=Amanda=sssss&age=32&&salary=1500&currency=euro"
-for smtng in long_string:
-    if "=" in long_string:
-        new_string = long_string.replace("=", " ")
-    if "&" in long_string:
-        new_string_2 = new_string.replace("&", " ")
-    if "s" in new_string_2:
-        new_string_3 = new_string_2.replace("s", "", 5)
-    if " " in new_string_3:
-        new_list = new_string_3.split()
-
-my_dict = {new_list[i]: new_list[i + 1] for i in range(0, len(new_list), 2)}
+filtered = long_string.replace("=", " ").replace("&", " ").replace("s", "", 5).split()
+my_dict = {filtered[i]: filtered[i + 1] for i in range(0, len(filtered), 2)}
 print(my_dict)
+
