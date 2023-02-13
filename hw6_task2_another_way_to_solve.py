e79bb882410194068e2975fe5484b85a761f6697
misha_fox <usmishafox13@gmail.com>
2023-02-13 11:34:31 +0400

    wh6 with no assert errors

---
+++ hw6_task2_another_way_to_solve.py
@@ -0,0 +1,27 @@
+"""Another way to solve"""
+
+from typing import Callable
+import math
+
+def find_perimetr(size):
+    perimetr = size * 4
+    return perimetr
+
+def find_area(size):
+    area = size * size
+    return area
+
+def find_diagonal(size):
+    diagonal = math.sqrt(2) * size
+    return diagonal
+
+def find_perimetr_and_area(callback: Callable, size):
+    return callback(size)
+
+list_of_data = [find_perimetr_and_area(find_perimetr, 7), find_perimetr_and_area(find_area, 7),
+                find_perimetr_and_area(find_diagonal, 7)]
+
+if __name__ == '__main__':
+
+    my_tuple = tuple(list_of_data)
+    print(my_tuple)
---
+++ hw6_task_4.py
@@ -0,0 +1,16 @@
+import re
+
+
+def remove_numbers(file):
+    with open(file, "r") as file_in:
+        contents = file_in.read()
+
+    no_numbers = re.sub(r'\d+', '', contents)
+    final_res = str(no_numbers)
+
+    with open(file, 'w') as file_out:
+        file_out.write(final_res)
+
+
+if __name__ == '__main__':
+    remove_numbers("some.txt")
