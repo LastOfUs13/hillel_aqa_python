a24c0fe84ddbe13e81702d18c1d4c186d2d6dcc2
misha_fox <usmishafox13@gmail.com>
2023-02-14 11:40:12 +0400

    small update

--- hw2_task3.py
+++ hw2_task3.py
@@ -3,3 +3,4 @@ poker_blacklist = {'Barry Allen', 'Ben Affleck', 'Selina Kyle'}
 alcohol_blacklist = {'Oliver Queen', 'Ben Affleck', 'Selina Kyle'}
 
 print(casino_blacklist.intersection(poker_blacklist, alcohol_blacklist))
+
