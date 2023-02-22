b46744863c225322891f53ea32477205af20412e
misha_fox <usmishafox13@gmail.com>
2023-02-22 18:55:06 +0400

    hw9

---
+++ hw9_task_1.py
@@ -0,0 +1,87 @@
+class Business():
+
+    def __init__(self, title=None, foundation_year=None, specialize=None, regional_offices=None,
+                 ceo=None):
+        self.__title = title
+        self.__foundation_year = foundation_year
+        self.__specialize = specialize
+        self.__regional_offices = regional_offices
+        self.__ceo = ceo
+
+    @property
+    def title(self):
+        return self.__title
+
+    @property
+    def foundation_year(self):
+        return self.__foundation_year
+
+    @property
+    def specialize(self):
+        return self.__specialize
+
+    @property
+    def regional_offices(self):
+        return self.__regional_offices
+
+    @property
+    def ceo(self):
+        return self.__ceo
+
+    @title.setter
+    def title(self, new_title):
+        if isinstance(new_title, str):
+            self.__title = new_title
+        else:
+            raise TypeError
+
+    @foundation_year.setter
+    def foundation_year(self, new_foundation_year):
+        if isinstance(new_foundation_year, int) and len(str(new_foundation_year)) == 4:
+            self.__foundation_year = new_foundation_year
+        else:
+            raise TypeError
+
+    @specialize.setter
+    def specialize(self, new_specialize):
+        if isinstance(new_specialize, str):
+            self.__specialize = new_specialize
+        else:
+            raise TypeError
+
+    @regional_offices.setter
+    def regional_offices(self, new_regional_offices):
+        if isinstance(new_regional_offices, str):
+            self.__regional_offices = new_regional_offices
+        else:
+            raise TypeError
+
+    @ceo.setter
+    def ceo(self, new_ceo):
+        if isinstance(new_ceo, str):
+            self.__ceo = new_ceo
+        else:
+            raise TypeError
+
+    @staticmethod
+    def tell_about_ceo():
+        return f"{company.ceo} was a engineer, and born leader."
+
+    @staticmethod
+    def tell_about_company():
+        return f"{company.title} was created in {company.foundation_year} by {company.ceo} and few his friends"
+
+    @staticmethod
+    def calculate_how_old_company():
+        import datetime
+        nova_days = datetime.datetime.now().year
+        res = nova_days - 1976
+        return f"Company {company.title} is a {res} years success continuous development"
+
+    @classmethod
+    def describe_company_shortly(cls, title, specialize, regional_offices):
+        return f"{title} specialized on {specialize},two most developed company regional offices in {regional_offices}"
+
+
+if __name__ == '__main__':
+    company = Business()
---
+++ hw9_task_2.py
@@ -0,0 +1,77 @@
+class Staff():
+
+    def __init__(self, name=None, gender=None, age=None, job_position=None, years_experience=None):
+        self.__name = name
+        self.__gender = gender
+        self.__age = age
+        self.__job_position = job_position
+        self.__years_experience = years_experience
+
+    @property
+    def name(self):
+        return self.__name
+
+    @property
+    def gender(self):
+        return self.__gender
+
+    @property
+    def age(self):
+        return self.__age
+
+    @property
+    def job_position(self):
+        return self.__job_position
+
+    @property
+    def years_experience(self):
+        return self.__years_experience
+
+    @name.setter
+    def name(self, staff_name):
+        if isinstance(staff_name, str):
+            self.__name = staff_name
+        else:
+            raise TypeError
+
+    @gender.setter
+    def gender(self, staff_gender):
+        if isinstance(staff_gender, str) and staff_gender == 'male' or staff_gender == 'female':
+            self.__gender = staff_gender
+        else:
+            raise TypeError
+
+    @age.setter
+    def age(self, staff_age):
+        condition = staff_age >= 18 and staff_age <= 65
+        if isinstance(staff_age, int) and condition:
+            self.__age = staff_age
+        else:
+            raise TypeError
+
+    @job_position.setter
+    def job_position(self, staff_job_position):
+        if isinstance(staff_job_position, str):
+            self.__job_position = staff_job_position
+        else:
+            raise TypeError
+
+    @years_experience.setter
+    def years_experience(self, staff_years_experience):
+        self.__years_experience = staff_years_experience
+
+    @staticmethod
+    def hi_to_new_staff():
+        return f"Say 'Hello' to our new {employee.job_position}-{employee.name}"
+
+    @classmethod
+    def create_new_staff(cls, job_position, name):
+        return f"{job_position} {name} was created"
+
+    def calculate_allowance_percentage(self):
+        allowance_percentage = self.years_experience * 0.5
+        return f"current allowance percentage for relevant employee is equal {allowance_percentage}"
+
+
+if __name__ == '__main__':
+    employee = Staff()
