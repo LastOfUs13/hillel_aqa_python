d4ced0f34a1da7956b73cfa691fb0f96111ea73f
misha_fox <usmishafox13@gmail.com>
2023-03-07 21:31:39 +0400

    upd

--- HW13/hw13.py
+++ HW13/hw13.py
@@ -9,7 +9,8 @@ class MyOwnIterator:
         return self
 
     def __next__(self):
-        if self.__start_index < self.__end_index and self.__start_index < len(self.__data):
+        if self.__start_index < self.__end_index and self.__start_index < len(self.__data) and len(
+                str(self.__start_index)) <= 1 and len(str(self.__end_index)) <= 1:
             value = self.__data[self.__start_index]
             self.__start_index += 1
             return value
@@ -18,8 +19,7 @@ class MyOwnIterator:
 
 
 if __name__ == '__main__':
-    iterator = MyOwnIterator([1, 2, 3, 4, 5, 6, 7, 8, 9], -1, 2)
+    iterator = MyOwnIterator([51, 23, 66, 15, 6, 18, 23, 10, 99], 5, 7)
 
     for items in iterator:
         print(items)
-
