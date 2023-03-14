e6b9b87e8aa4c20cd8061e2cfcf6e604cc484831
LastOfUs13 <122573180+LastOfUs13@users.noreply.github.com>
2023-03-14 12:05:21 +0400

    Delete HW14/proxy directory

--- HW14/proxy/txt_reader.py
+++
@@ -1,8 +0,0 @@
-class TxtReader:
-    def __init__(self, file_path):
-        self.__file_path = file_path
-
-    def read_file(self):
-        with open(self.__file_path) as file:
-            text = file.read()
-        return text
